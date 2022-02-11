e3350e360f10cb6fdb3cff07b50f154ec9405d42
Keith Benjamin <kjb528i@yahoo.com>
2022-02-11 18:24:41 -0500

    completed B_02_7 encapsulation

---
+++ src/com/careerdevs/interm/GettingStartedB.java
@@ -0,0 +1,43 @@
+package com.careerdevs.interm;
+
+public class GettingStartedB {
+
+    public static void B_01_02(){
+        /*
+        programming paradigms:
+        procedural
+        **functional  => keeps data and behaviour separate
+        **oop  => keeps data and behaviour together as objects
+        event-driven
+        logic
+        aspect-oriented
+
+        OOP
+        has a state => data
+        has a behaviour => method
+         this makes up a single object
+         */
+    }
+
+    public static void B_01_03(){
+        /*
+        objects => objects are reusable => think of car parts
+        smaller objects => small chunks => reuse code
+        faster development
+        you need to know how to do OOP properly
+         */
+    }
+    public static void B_01_04(){
+        /*
+        classes
+        encapsulation
+        abstraction
+        *classes
+        refactoring towards OO code
+        inheritance and polymorphism
+        interfaces
+         */
+    }
+
+
+}
---
+++ src/com/careerdevs/interm/Interfaces.java
@@ -0,0 +1,4 @@
+package com.careerdevs.interm;
+
+public class Interfaces {
+}
